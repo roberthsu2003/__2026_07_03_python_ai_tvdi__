48c197f360b6aa6fbd84a8179412dcfc42d2e393
roberthsu2003 <roberthsu2003@gmail.com>
2026-07-20 14:19:06 +0800

    modify

--- backend/07_20/practice2.py
+++ backend/07_20/practice2.py
@@ -1,17 +1,19 @@
 from fastapi import FastAPI
 import uvicorn
+from source import get_camera_position, CameraPosition
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    data:list[CameraPosition] = get_camera_position()
+    return data
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+#@app.get("/items/{item_id}")
+#def read_item(item_id: int, q: str | None = None):
+#    return {"item_id": item_id, "q": q}
 
 if __name__ == "__main__":
     uvicorn.run("practice2:app",reload=True)
