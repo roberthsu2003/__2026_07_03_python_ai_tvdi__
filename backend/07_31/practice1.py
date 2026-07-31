a39f1228123d575dadd85f16540c5d437e7cc555
roberthsu2003 <roberthsu2003@gmail.com>
2026-07-31 09:24:53 +0800

    modify

--- backend/07_31/practice1.py
+++ backend/07_31/practice1.py
@@ -18,6 +18,14 @@ app = FastAPI(
 def read_root():
     return {"message": "歡迎來到 FastAPI 主頁！請存取 /ui 使用 Gradio 介面。"}
 
+@app.get("/api/greet")
+def api_greet(name:str="World"):
+    """一個簡單的原生 FastAPI 端點"""
+    return {
+        "status":"success",
+        "result":f"Hello, {name} from FastAPI"
+        }
+
 if __name__ == "__main__":
     # 執行伺服器：http://0.0.0.0:8000
     uvicorn.run(app, host="0.0.0.0", port=8000)
