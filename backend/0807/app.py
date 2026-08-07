a40130fc689b400dd5bbb30dfacdb1202bd6763d
roberthsu2003 <roberthsu2003@gmail.com>
2026-08-07 10:48:01 +0800

    modify

--- backend/0807/app.py
+++ backend/0807/app.py
@@ -1,2 +1,11 @@
+import os
+import sys
+from train_save import train_and_save_model
+
+current_dir = os.getcwd()
+if current_dir not in sys.path:
+    sys.path.insert(0, current_dir)
+
+
 if __name__ == "__main__":
     print("Hello!")
